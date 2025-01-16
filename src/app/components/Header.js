a9c9abcefc582835e31e44d1c2ea69e5579d5ca7
Daniel Cassell <danielcassell@Mac.ultrahub>
2025-01-16 22:05:20 +0000

    Add Header component

---
+++ src/app/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="w-100% text-center">
+      <h1 className="text-pastelBlue-dark text-4xl font-bold">
+        Poppet and Boo
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
+import Header from "./components/Header";
+
 export default function Home() {
   return (
     <div className="w-screen h-screen">
-      <h1 className="text-pastelBlue-dark font-bold text-4xl">
-        Poppet and Boo
-      </h1>
+      <Header />
     </div>
   );
 }

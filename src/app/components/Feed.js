00d1744f5fc11d17dafd0099b63da64b4ef7f1d9
Daniel Cassell <danielcassell@Mac.ultrahub>
2025-01-19 18:16:27 +0000

    Add masonry styling

--- src/app/components/Feed.js
+++ src/app/components/Feed.js
@@ -3,6 +3,7 @@ import { getPosts } from "@/lib/contentful";
 import { useEffect, useState } from "react";
 import Post from "./Post.js";
 import AddPostButton from "./AddPostButton.js";
+import Masonry from "react-masonry-css"; // Import Masonry
 
 const Feed = () => {
   const [posts, setPosts] = useState([]);
@@ -20,14 +21,28 @@ const Feed = () => {
     fetchPosts();
   }, [reRender]);
 
+  const masonryBreakpoints = {
+    default: 4, // Number of columns for large screens
+    1100: 3, // Number of columns for screens smaller than 1100px
+    768: 2, // Number of columns for screens smaller than 768px
+    480: 1, // Number of columns for screens smaller than 480px
+  };
+
   return (
-    <div className="flex relative flex-col items-center overflow-scroll h-screen">
-      <div className="w-100% mx-4 md:w-2/5 md:mx-0">
+    <div className="relative overflow-scroll h-screen p-4">
+      {/* Masonry Layout */}
+      <Masonry
+        breakpointCols={masonryBreakpoints}
+        className="flex -ml-4 w-auto"
+        columnClassName="pl-4 bg-clip-padding"
+      >
         {posts.map((post, index) => (
-          <Post post={post} key={index}></Post>
+          <div key={index} className="mb-4">
+            <Post post={post} />
+          </div>
         ))}
-      </div>
-      {/* Pass addNewPost as a prop to AddPostButton */}
+      </Masonry>
+      {/* Add Post Button */}
       <AddPostButton setReRender={setReRender} reRender={reRender} />
     </div>
   );

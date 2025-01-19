8162de78246fe09adcbb9c23ea6e826fc29a1dce
Daniel Cassell <danielcassell@Mac.ultrahub>
2025-01-19 21:17:33 +0000

    Add close modal

--- src/app/components/PostWithImage.js
+++ src/app/components/PostWithImage.js
@@ -216,6 +216,13 @@ const PostWithImage = ({ post }) => {
             className="max-w-full max-h-full object-contain"
             onClick={(e) => e.stopPropagation()} // Prevent closing modal when clicking the image
           />
+          {/* Close Button */}
+          <button
+            onClick={() => setIsModalOpen(false)}
+            className="absolute w-8 h-8 top-4 right-4 bg-white text-black rounded-full shadow-lg"
+          >
+            &times; {/* Close icon */}
+          </button>
         </div>
       )}
     </div>
